f1923914586658870f4c0926d223636d691b35f5
nyx-Anna <jhaanju236@gmail.com>
2026-04-12 23:21:43 +0530

    added new file

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,21 @@ import { useState, useEffect } from "react";
 import TaskForm from "./components/TaskForm";
 import TaskList from "./components/TaskList";
 import TaskItem from "./components/TaskItem";
+import { useContext } from "react";
+import { ThemeContext } from "./context/ThemeContext";
 
 function App() {
   // state to store tasks
-  // const [tasks, setTasks] = useState([]);
+  const { darkMode, toggleTheme } = useContext(ThemeContext);
   const [tasks, setTasks] = useState(() => {
     const savedTasks = localStorage.getItem("tasks");
     return savedTasks ? JSON.parse(savedTasks) : [];
+  });
+  const [filter, setFilter] = useState("all");
+  const filteredTasks = tasks.filter((task) => {
+    if (filter === "completed") return task.completed;
+    if (filter === "pending") return !task.completed;
+    return true;
   });
 
   // function to add task
@@ -46,17 +54,60 @@ function App() {
   }, [tasks]);
 
   return (
-    <div className="min-h-screen bg-linear-to-br from-indigo-200 to-purple-200 flex items-center justify-center">
+    <div
+      className={`min-h-screen flex items-center justify-center ${
+        darkMode
+          ? "bg-gray-900 text-white"
+          : "bg-linear-to-br from-indigo-200 to-purple-200"
+      }`}
+    >
       {/* Card Container */}
-      <div className="bg-white p-6 rounded-2xl shadow-lg w-full max-w-md">
+      <div
+        className={`p-6 rounded-2xl shadow-lg w-full max-w-md ${
+          darkMode ? "bg-gray-800" : "bg-white"
+        }`}
+      >
+        <button
+          onClick={toggleTheme}
+          className="mb-3 px-4 py-2 rounded-lg shadow bg-indigo-500 hover:bg-indigo-600 transition"
+        >
+          {darkMode ? "🌞" : "🌙"}
+        </button>
         <h1 className="text-2xl font-bold text-center mb-4">Task Manager</h1>
+        <p className="text-center text-indigo-600 mb-3">
+          Total: {tasks.length} | Completed:{" "}
+          {tasks.filter((t) => t.completed).length}
+        </p>
 
         {/* form */}
         <TaskForm addTask={addTask} />
 
+        <div className="flex justify-center gap-2 mb-4">
+          <button
+            onClick={() => setFilter("all")}
+            className="px-3 py-1 bg-gray-300 rounded"
+          >
+            All
+          </button>
+
+          <button
+            onClick={() => setFilter("completed")}
+            className="px-3 py-1 bg-green-300 rounded"
+          >
+            Completed
+          </button>
+
+          <button
+            onClick={() => setFilter("pending")}
+            className="px-3 py-1 bg-yellow-300 rounded"
+          >
+            Pending
+          </button>
+        </div>
+
         {/* list */}
         <TaskList
-          tasks={tasks}
+          tasks={filteredTasks}
           deleteTask={deleteTask}
           toggleTask={toggleTask}
           editTask={editTask}
